81dfbfdfddd940eb466089d11b3073556c11f482
AndroidRul3z <ferrettidario@gmail.com>
2013-03-21 20:42:07 -0500

    Purity dark UI mode toggle
    - Credit to Slimroms
    - Remove qs tile background
    - TODO: add inverted quick launch navbar icons

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4563,6 +4563,13 @@ public final class Settings {
         public static final String UI_NIGHT_MODE = "ui_night_mode";
 
         /**
+         * Whether user activated inverted UI mode or default UI mode. Owned
+         * and controlled by UiModeManagerService.
+         * @hide
+         */
+        public static final String UI_INVERTED_MODE = "ui_inverted_mode";
+
+        /**
          * Whether screensavers are enabled.
          * @hide
          */
@@ -4659,6 +4666,7 @@ public final class Settings {
             MOUNT_UMS_PROMPT,
             MOUNT_UMS_NOTIFY_ENABLED,
             UI_NIGHT_MODE,
+            UI_INVERTED_MODE,
             DIALPAD_AUTOCOMPLETE
         };
 
