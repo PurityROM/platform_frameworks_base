47142e6df1f571fe5a185cb2a85fa29817bb06cc
Lars Greiss <kufikugel@googlemail.com>
2013-05-11 21:50:34 +0200

    Framworks: disable observer and receiver if PIE is disabled
    on high end devices that is maybe not needed. But there
    is in general no need to enable the obeserver/receiver
    when PIE is disabled (or expanded mode is off)
    This change destroy and disables the PIE in most parts
    when it is disabled. + a small doubel register
    correction
    
    greetings SlimRoms
    
    PatchSet 2:
    - remove PieLayout observer completly - thanks Jens
    
    PatchSet 3:
    - remove unnecessary call to setupSnapPoints()
    - reuse already created snap points
    
    PatchSet 4:
    - small corrections on setupSnapPoints() and call
    - remove not needed second call of PieController attachTo()
    - disable TelephonyManager listener
    
    PatchSet 5:
    - fix up detach logic
    
    PatchSet 7:
    - cleanup
    
    PatchSet 8:
    - Effective Java rule #41
    - fixed some pie controller leak
    
    Change-Id: I8ef8797e7d4f8ee73d31a8296f5747415567bbda

--- packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
@@ -409,15 +409,10 @@ public abstract class BaseStatusBar extends SystemUI implements
             }
         }, filter);
 
-        mPieController = new PieController(mContext);
-        mPieController.attachTo(this);
-        addNavigationBarCallback(mPieController);
-
         mSettingsObserver = new PieSettingsObserver(new Handler());
 
         // this calls attachPie() implicitly
         mSettingsObserver.onChange(true);
-
         mSettingsObserver.observe();
     }
 
@@ -1412,7 +1407,13 @@ public abstract class BaseStatusBar extends SystemUI implements
                 lp.screenOrientation = ActivityInfo.SCREEN_ORIENTATION_BEHIND;
 
                 mWindowManager.addView(mPieContainer, lp);
-                mPieController.attachTo(mPieContainer);
+                // once we need a pie controller, we create one and keep it forever ...
+                if (mPieController == null) {
+                    mPieController = new PieController(mContext);
+                    mPieController.attachStatusBar(this);
+                    addNavigationBarCallback(mPieController);
+                }
+                mPieController.attachContainer(mPieContainer);
             }
 
             // add or update pie triggers
@@ -1429,6 +1430,11 @@ public abstract class BaseStatusBar extends SystemUI implements
                     mWindowManager.removeView(mPieTrigger[i]);
                     mPieTrigger[i] = null;
                 }
+            }
+            // detach from the pie container and unregister observers and receivers
+            if (mPieController != null) {
+                mPieController.detachContainer();
+                mPieContainer = null;
             }
         }
     }
