8e4a62898d77b2ced16ff950f4d1f9588ace3a46
Danny Baumann <dannybaumann@web.de>
2013-09-18 09:29:55 +0200

    Fix dock intent lookup.
    PackageManagerService doesn't support UserHandle.USER_CURRENT passed in,
    so always pass in the real user ID.
    
    Change-Id: I86ed3cc41ee52628da874500d280f722501233cf

--- policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
+++ policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
@@ -299,6 +299,8 @@ public class PhoneWindowManager implements WindowManagerPolicy {
     int mBackKillTimeout;    
     int mPointerLocationMode = 0; // guarded by mLock
 
+    int mCurrentUser = 0;
+
     // The last window we were told about in focusChanged.
     WindowState mFocusedWindow;
     IApplicationToken mFocusedApp;
@@ -4935,8 +4937,8 @@ public class PhoneWindowManager implements WindowManagerPolicy {
         ActivityInfo ai = null;
         ResolveInfo info = mContext.getPackageManager().resolveActivityAsUser(
                 intent,
-                PackageManager.MATCH_DEFAULT_ONLY,
-                UserHandle.USER_CURRENT);
+                PackageManager.MATCH_DEFAULT_ONLY | PackageManager.GET_META_DATA,
+                mCurrentUser);
         if (info != null) {
             ai = info.activityInfo;
         }
@@ -5179,6 +5181,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
             }
         }
         setLastInputMethodWindowLw(null, null);
+        mCurrentUser = newUserId;
     }
 
     @Override
